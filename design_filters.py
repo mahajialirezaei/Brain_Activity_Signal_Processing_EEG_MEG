5bcb4c30c51d847f031bf41e70ca0facd3840504
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-14 23:35:30 +0330

    import needed libs

--- design_filters.py
+++ design_filters.py
@@ -1,5 +1,10 @@
 import load_data
+from scipy.signal import freqz
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 subjects = load_data.getSubject()
 runs = load_data.getRun()
 base_dir = load_data.getbase_dir()
+
