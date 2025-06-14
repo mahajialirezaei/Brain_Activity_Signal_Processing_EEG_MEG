f6f2a456170938285f231ec5857ed81f3f6c7244
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-14 23:40:49 +0330

    define determine HandW

--- design_filters.py
+++ design_filters.py
@@ -1,3 +1,4 @@
+import os
 import load_data
 from scipy.signal import freqz
 import numpy as np
@@ -7,4 +8,19 @@ import matplotlib.pyplot as plt
 subjects = load_data.getSubject()
 runs = load_data.getRun()
 base_dir = load_data.getbase_dir()
+
+
+def determineHandW():
+    ls = []
+    for subject in subjects:
+        subject_dir = os.path.join(base_dir, subject)
+
+        for run in runs:
+            edf_file = os.path.join(subject_dir, f"{subject}{run}.edf")
+
+            b, a = load_data.process_edf_file(edf_file)['filter_coeffs'].values()
+            ls.append((b, a))
+
+    return ls
+
 
